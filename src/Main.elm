96cba9452308a4ade80735c18dd25bf323efc48f
Tantatorn <timefear007@gmail.com>
2024-09-07 14:25:12 +0700

    ✨ add background

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
 
 -- MAIN
@@ -11,11 +12,14 @@ main =
 
 -- MODEL
 
-type alias Model = Int
+type alias Model = { length: Int, posX: Int, posY: Int }
 
 init : Model
-init =
-  0
+init = {
+  length = 4
+  , posX = 15
+  , posY = 15 
+  }
 
 
 -- UPDATE
@@ -25,19 +29,13 @@ type Msg = Increment | Decrement
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Increment ->
-      model + 1
-
-    Decrement ->
-      model - 1
-
+    Increment -> { model | length = model.length + 1 }
+    Decrement -> { model | length = model.length - 1 }
 
 -- VIEW
-
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
+  div [ style "background" "#d3d3d3", style "width" "30rem", style "height" "30rem" ]
+    [ 
+    div [] [ text (String.fromInt model.length) ]
     ]
